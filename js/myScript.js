32952706309ee4758465948fa88fddd4038ccc4b
Stanislav Bolsun <bolsun@arrival.com>
2023-07-16 11:42:13 +0200

    re-init

--- js/myScript.js
+++
@@ -1,8 +0,0 @@
-function myTimer() {
-    var date = new Date();
-    var options ={
-        hour: 'numeric',
-        minute: 'numeric'
-    };
-    document.getElementById("time").innerText = date.toLocaleString("ru",options);
-}
